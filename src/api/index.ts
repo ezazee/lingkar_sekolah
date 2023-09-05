733451a2040d190f3aac399dd98e76f7024c10a9
ezazee <yukiirima123@gmail.com>
2023-09-05 09:37:53 +0800

    RV: Adding API Endpoint

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
-// API KEY NEWS
-// 1c8dc2f4d1724e4eb7ad10a7e8ab0af5
 export const apiProfile: string =
   'https://64ee9301219b3e2873c354a9.mockapi.io/APIAvatar';
+
+export const apiBlog: string =
+  'https://64ee9301219b3e2873c354a9.mockapi.io/apiBlog';
